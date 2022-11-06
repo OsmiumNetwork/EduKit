f8383f6c57358faaf0f4a3f0493b6b01be980126
FireStreaker2 <103970465+FireStreaker2@users.noreply.github.com>
2022-11-05 18:09:40 -0700

    Create cloak.js

---
+++ cloak.js
@@ -0,0 +1,44 @@
+function openInNewTab(url) {
+
+
+  win = window.open();
+  win.document.body.style.margin = '0';
+  win.document.body.style.height = '100vh';
+
+  var icon = win.document.createElement('link')
+  icon.rel = "shortcut icon"
+  icon.href = "https://www.fishme.me/icon.png"
+  icon.type = "image/png"
+  win.document.head.appendChild(icon)
+  
+  var arc = win.document.createElement('script')
+  arc.async = true
+  arc.src = "https://arc.io/widget.min.js#RqQiAHK7"
+  win.document.head.appendChild(arc)
+
+  var title = win.document.createElement('title')
+  title.innerText = "Calculator"
+  win.document.head.appendChild(title)
+
+  /*
+  var home = win.document.createElement('a')
+  home.style.display = "block"
+  home.style.position = "absolute"
+  home.style.fontFamily = "'Courier New', Courier, monospace";
+  home.style.color = "rgb(76, 160, 55)"
+  home.href="#"
+  home.onclick = 'return console.log("hi") && false'
+  home.innerText = "home"
+  win.document.body.appendChild(home);
+  */
+
+  var iframe = win.document.createElement('iframe');
+  iframe.style.border = 'none';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.margin = '0';
+  iframe.id = 'content';
+  iframe.src = url;
+  win.document.body.appendChild(iframe);
+
+}
