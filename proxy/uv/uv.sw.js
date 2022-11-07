1c350aec0a4e0ed811ff97161141b1bee14eb34f
The Greatest Giant <vps@drshacker.ml>
2022-11-06 20:46:49 -0500

    Update uv.sw.js

--- proxy/uv/uv.sw.js
+++ proxy/uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
+importScripts('/proxy/uv/uv.bundle.js');
+importScripts('/proxy/uv/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
