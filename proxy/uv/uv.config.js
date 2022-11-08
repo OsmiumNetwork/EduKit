c8768bcc90b906690fb4ba63a2726ab0df6e36bd
FireStreaker2 <103970465+FireStreaker2@users.noreply.github.com>
2022-11-07 18:01:11 -0800

    Update uv.config.js

--- proxy/uv/uv.config.js
+++ proxy/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/proxy/static/uv/uv.handler.js',
-    bundle: '/proxy/static/uv/uv.bundle.js',
-    config: '/proxy/static/uv/uv.config.js',
-    sw: '/proxy/static/uv/uv.sw.js',
+    handler: '/proxy/uv/uv.handler.js',
+    bundle: '/proxy/uv/uv.bundle.js',
+    config: '/proxy/uv/uv.config.js',
+    sw: '/proxy/uv/uv.sw.js',
 };
