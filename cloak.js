fca832c6460d9b1004d6d91eb51667d7197b2081
The Greatest Giant <vps@drshacker.ml>
2022-11-10 22:20:18 -0500

    Update cloak.js

--- cloak.js
+++ cloak.js
@@ -1,3 +1,6 @@
+function rr(){
+ document.body.innerHTML='<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+}
 function openInNewTab(url) {
 
 
