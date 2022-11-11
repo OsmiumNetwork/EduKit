76b100289ca293d1b883f7bda19663ae1569a4df
The Greatest Giant <vps@drshacker.ml>
2022-11-10 22:43:32 -0500

    Update cloak.js

--- cloak.js
+++ cloak.js
@@ -1,5 +1,10 @@
 function rr(){
- document.body.innerHTML='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+ document.body.innerHTML=`
+ <video width="100%" height="100%" autoplay>
+  <source src="https://osmiumnetwork.github.io/EduKit/roll.mp4" type="video/mp4">
+  Your browser does not support HTML5 video.
+</video>
+ `;
 }
 function openInNewTab(url) {
 
