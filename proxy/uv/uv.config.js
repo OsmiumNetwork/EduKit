57ff8cae2397d7f6f0282635d862a843861a87ca
schooldev49 <102310270+schooldev49@users.noreply.github.com>
2022-11-12 13:19:02 -0500

    Update uv.config.js

--- proxy/uv/uv.config.js
+++ proxy/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/EduKit/proxy/static/~/',
-    bare: '/bare/',
+    bare: 'https://edukitbackend.up.railway.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/EduKit/proxy/uv/uv.handler.js',
