b7c66ee6fc615cbdb85e4f906017c733b4a19f85
FireStreaker2 <103970465+FireStreaker2@users.noreply.github.com>
2022-11-15 18:06:51 -0800

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/proxy/static/~/',
+    prefix: '/service/',
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/EduKit/proxy/uv/uv.handler.js',
-    bundle: '/EduKit//proxy/uv/uv.bundle.js',
-    config: '/EduKit/proxy/uv/uv.config.js',
-    sw: '/EduKit/proxy/uv/uv.sw.js',
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: 'uv.sw.js',
 };
